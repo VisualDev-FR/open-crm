f52e328f3464fefe88b0926b8ba8c943d772437b
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2025-09-22 18:38:26 +0200

    back: configure SQL Server

---
+++ back/src/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using OpenCRM.Entities;
+
+namespace OpenCRM;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Client> Clients { get; set; }
+}
---
+++ back/src/Entities/Client.cs
@@ -0,0 +1,10 @@
+namespace OpenCRM.Entities;
+
+public class Client
+{
+    public int Id { get; set; }
+
+    public string Nom { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+}
