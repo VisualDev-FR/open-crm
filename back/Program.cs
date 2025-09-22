001010b1ef15aa1fcb951f908147f2ac494d3cbf
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2025-09-22 18:58:46 +0200

    back: setup ClientController.cs

--- back/Program.cs
+++ back/Program.cs
@@ -11,6 +11,9 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
+app.MapControllers();
 app.Run();
---
+++ back/src/Controllers/ClientController.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace OpenCRM.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class ClientController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ClientController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var clients = await _context.Clients.ToListAsync();
+        return Ok(clients);
+    }
+}
