f59cd4392740c53961cd0733a3f9ed8a7b3ea86b
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2025-09-23 21:47:49 +0200

    back: remove JwtExtensions to merge it into IServiceCollectionExtensions

--- back/src/Extensions/IServiceCollectionExtensions.cs
+++ back/src/Extensions/IServiceCollectionExtensions.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Linq;
 using System.Reflection;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using OpenCRM.Attributes;
 
 namespace OpenCRM.Extensions;
@@ -16,5 +20,31 @@ public static class IServiceCollectionExtensions
         {
             service.AddScoped(type);
         }
+    }
+
+    public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        string jwtKey = configuration.SafeGet("Jwt:Key");
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = configuration.SafeGet("Jwt:Issuer"),
+                ValidAudience = configuration.SafeGet("Jwt:Audience"),
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+            };
+        });
+
+        return services;
     }
 }
